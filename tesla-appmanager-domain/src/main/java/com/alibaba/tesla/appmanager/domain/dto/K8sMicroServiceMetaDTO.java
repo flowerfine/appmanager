6946f50051d04cdeba024d2588db4d2f0fd91cb5
wangqi <wangqi@xinxuan.net>
2025-05-27 21:52:27 +0800

    feature: add oam model

--- tesla-appmanager-domain/src/main/java/com/alibaba/tesla/appmanager/domain/dto/K8sMicroServiceMetaDTO.java
+++ tesla-appmanager-domain/src/main/java/com/alibaba/tesla/appmanager/domain/dto/K8sMicroServiceMetaDTO.java
@@ -1,6 +1,5 @@
 package com.alibaba.tesla.appmanager.domain.dto;
 
-import com.alibaba.tesla.appmanager.common.enums.ComponentTypeEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
