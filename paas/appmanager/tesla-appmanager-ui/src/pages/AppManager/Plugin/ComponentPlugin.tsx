8ec0aa90e6b6cf806cd7f44ac3ccb677d471b91a
wangqi <wangqi@xinxuan.net>
2025-05-28 21:02:01 +0800

    feature: update appmanager dynamic script

--- paas/appmanager/tesla-appmanager-ui/src/pages/AppManager/Plugin/ComponentPlugin.tsx
+++ paas/appmanager/tesla-appmanager-ui/src/pages/AppManager/Plugin/ComponentPlugin.tsx
@@ -3,18 +3,29 @@ import { AppManagerAPI } from "@/services/appmanager/typings";
 import { EditOutlined } from "@ant-design/icons";
 import { ActionType, PageContainer, ProColumns, ProFormInstance, ProTable } from "@ant-design/pro-components";
 import { useIntl } from "@umijs/max";
-import { Alert, Button, Space, Switch, Tooltip } from "antd";
-import { FC, useRef } from "react";
+import { Alert, Button, Modal, Space, Switch, Tooltip } from "antd";
+import { FC, useRef, useState } from "react";
+
+type DefinitionSchemaState = {
+  show: boolean;
+  schema?: string;
+}
 
 const ComponentPlugin: FC = () => {
   const intl = useIntl();
   const actionRef = useRef<ActionType>();
   const formRef = useRef<ProFormInstance>();
 
+  const [showSchema, setShowSchema] = useState<DefinitionSchemaState>({ show: false });
+
   const columns: ProColumns<AppManagerAPI.Plugin>[] = [
     {
       title: "名称",
       dataIndex: 'pluginName'
+    },
+    {
+      title: "描述",
+      dataIndex: 'pluginDescription'
     },
     {
       title: "版本",
@@ -29,18 +40,32 @@ const ComponentPlugin: FC = () => {
         <Switch checked={record.pluginRegistered} />
       ),
       width: 160,
+    },
+    {
+      title: "操作",
+      valueType: 'option',
+      width: 160,
+      render: (_, record) => (
+        <Space>
+          <Button onClick={() => {
+            setShowSchema({ show: true, schema: record.pluginSchema });
+          }}>
+            查看
+          </Button>
+        </Space>
+      ),
     }
   ];
 
   return (
     <PageContainer breadcrumbRender={false} title={false} content={
       <Alert message="Component说明"
-      description="为您的应用程序提供可扩展的工作负载(workload)组件，内置提供通用微服务组件、helm组件，您也可以自己扩展更多的workload类型"
-      type="warning"
-      showIcon
-      closable
+        description="为您的应用程序提供可扩展的工作负载(workload)组件，内置提供通用微服务组件、helm组件，您也可以自己扩展更多的workload类型"
+        type="warning"
+        showIcon
+        closable
       />}
-      >
+    >
       <ProTable<AppManagerAPI.Plugin>
         search={false}
         rowKey="id"
@@ -50,6 +75,7 @@ const ComponentPlugin: FC = () => {
         pagination={{ showQuickJumper: true, showSizeChanger: true, defaultPageSize: 10 }}
         params={{
           pluginKind: 'ComponentDefinition',
+          withBlobs: true
         }}
         request={(params, sorter, filter) => {
           const queryParam: AppManagerAPI.PluginQueryParam = {
@@ -62,6 +88,20 @@ const ComponentPlugin: FC = () => {
         }}
         options={false}
       />
+
+      {showSchema.show && <Modal
+        width={'50%'}
+        open={showSchema.show}
+        destroyOnHidden={true}
+        onOk={() => {
+          setShowSchema({ show: false });
+        }}
+        onCancel={() => {
+          setShowSchema({ show: false });
+        }}
+      >
+        <pre>{showSchema.schema}</pre>
+      </Modal>}
     </PageContainer>
   )
 }
--- paas/appmanager/tesla-appmanager-ui/src/pages/AppManager/Plugin/TraitPlugin.tsx
+++ paas/appmanager/tesla-appmanager-ui/src/pages/AppManager/Plugin/TraitPlugin.tsx
@@ -3,18 +3,29 @@ import { AppManagerAPI } from "@/services/appmanager/typings";
 import { EditOutlined } from "@ant-design/icons";
 import { ActionType, PageContainer, ProColumns, ProFormInstance, ProTable } from "@ant-design/pro-components";
 import { useIntl } from "@umijs/max";
-import { Alert, Button, Space, Switch, Tooltip } from "antd";
-import { FC, useRef } from "react";
+import { Alert, Button, Modal, Space, Switch, Tooltip } from "antd";
+import { FC, useRef, useState } from "react";
+
+type DefinitionSchemaState = {
+  show: boolean;
+  schema?: string;
+}
 
 const TraitPlugin: FC = () => {
   const intl = useIntl();
   const actionRef = useRef<ActionType>();
   const formRef = useRef<ProFormInstance>();
 
+  const [showSchema, setShowSchema] = useState<DefinitionSchemaState>({ show: false });
+
   const columns: ProColumns<AppManagerAPI.Plugin>[] = [
     {
       title: "名称",
       dataIndex: 'pluginName'
+    },
+    {
+      title: "描述",
+      dataIndex: 'pluginDescription'
     },
     {
       title: "版本",
@@ -29,18 +40,32 @@ const TraitPlugin: FC = () => {
         <Switch checked={record.pluginRegistered} />
       ),
       width: 160,
+    },
+    {
+      title: "操作",
+      valueType: 'option',
+      width: 160,
+      render: (_, record) => (
+        <Space>
+          <Button onClick={() => {
+            setShowSchema({ show: true, schema: record.pluginSchema });
+          }}>
+            查看
+          </Button>
+        </Space>
+      ),
     }
   ];
 
   return (
     <PageContainer breadcrumbRender={false} title={false} content={
       <Alert message="Trait说明"
-      description="为您的应用程序提供可扩展的运维特性能力"
-      type="warning"
-      showIcon
-      closable
+        description="为您的应用程序提供可扩展的运维特性能力"
+        type="warning"
+        showIcon
+        closable
       />}
-      >
+    >
       <ProTable<AppManagerAPI.Plugin>
         search={false}
         rowKey="id"
@@ -50,6 +75,7 @@ const TraitPlugin: FC = () => {
         pagination={{ showQuickJumper: true, showSizeChanger: true, defaultPageSize: 10 }}
         params={{
           pluginKind: 'TraitDefinition',
+          withBlobs: true
         }}
         request={(params, sorter, filter) => {
           const queryParam: AppManagerAPI.PluginQueryParam = {
@@ -62,6 +88,20 @@ const TraitPlugin: FC = () => {
         }}
         options={false}
       />
+
+      {showSchema.show && <Modal
+        width={'50%'}
+        open={showSchema.show}
+        destroyOnHidden={true}
+        onOk={() => {
+          setShowSchema({ show: false });
+        }}
+        onCancel={() => {
+          setShowSchema({ show: false });
+        }}
+      >
+        <pre>{showSchema.schema}</pre>
+      </Modal>}
     </PageContainer>
   )
 }
